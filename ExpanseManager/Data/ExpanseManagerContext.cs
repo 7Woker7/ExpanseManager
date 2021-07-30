92208ce4653f24174a90ddbe9b24730069b5e494
7Woker7 <83217918+7Woker7@users.noreply.github.com>
2021-07-30 10:39:24 +0300

    Created database

---
+++ ExpanseManager/Data/ExpanseManagerContext.cs
@@ -0,0 +1,20 @@
+﻿using ExpanseManager.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpanseManager.Data
+{
+    public class ExpanseManagerContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(local);Database=ExpanceManager;Trusted_Connection=True;");
+        }
+    }
+
+}
